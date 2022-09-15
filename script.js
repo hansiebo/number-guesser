7e766752952dfa931fa44d802b6b315eb97c705a
Han Siebo <han.siebo@proton.me>
2022-09-15 12:44:07 +0200

    Add JS functions

--- script.js
+++ script.js
@@ -3,4 +3,30 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+    return Math.floor(Math.random() * 10);
+}
 
+
+function compareGuesses(human, computer, target) {
+    if (Math.abs(target - human) <= Math.abs(target - computer) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateScore(winner) {
+    if (winner === "human") {
+        humanScore++;
+    } else if (winner === "computer") {
+        computerScore++;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+}
+
+//Test
+//console.log(generateTarget());
